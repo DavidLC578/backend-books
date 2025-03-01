893723fb302085918f308a9cdd1bb2d778e8d5bb
DavidLC578 <davidlatt2002@gmail.com>
2025-02-28 19:39:18 -0500

    fix: longtext to synopsis

--- database/migrations/2025_02_13_200834_create_books_table.php
+++ database/migrations/2025_02_13_200834_create_books_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('user_id')
                 ->constrained()
                 ->onDelete('cascade')->nullable();
-            $table->string('synopsis');
+            $table->longText('synopsis');
             $table->string('category');
             $table->string('file')->nullable();
             $table->string('image')->nullable();
